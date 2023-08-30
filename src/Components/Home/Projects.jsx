8cc04fe67ab42ed016360ea0793d1578be8e7157
Manjunath <35862575+manjunath1704@users.noreply.github.com>
2023-08-30 13:38:40 +0530

    updates

--- src/Components/Home/Projects.jsx
+++ src/Components/Home/Projects.jsx
@@ -1,32 +1,79 @@
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay, EffectCoverflow } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import { createMedia } from "@artsy/fresnel";
 
+const projectsData = [
+  {
+    projectLink:
+      "https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles",
+    backgroundImage:
+      "https://d2kq0urxkarztv.cloudfront.net/62c2eecea67ec50023695793/4046480/image-47ceafde-6060-4820-9bee-c81047e2a5b6.png?h=612&e=webp&nll=true",
+  },
+  {
+    projectLink:
+      "https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles",
+    backgroundImage:
+      "https://d2kq0urxkarztv.cloudfront.net/62c2eecea67ec50023695793/4046480/image-47ceafde-6060-4820-9bee-c81047e2a5b6.png?h=612&e=webp&nll=true",
+  },
+  {
+    projectLink:
+      "https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles",
+    backgroundImage:
+      "https://d2kq0urxkarztv.cloudfront.net/62c2eecea67ec50023695793/4046480/image-b65a7fba-e69d-4c95-a0a7-d359444f254a.png?h=612&e=webp&nll=true",
+  },
+  {
+    projectLink:
+      "https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles",
+    backgroundImage:
+      "https://d2kq0urxkarztv.cloudfront.net/62c2eecea67ec50023695793/4046480/image-d2556dfa-cc47-4521-b6b9-d671ebde75b8.png?h=612&e=webp&nll=true",
+  },
+];
+const SlideImage = ({ backgroundImage, projectLink }) => {
+  return (
+    <a href={projectLink} className="d-block">
+      <div className="sid-projects__slide">
+      <img
+              src={backgroundImage}
+              alt=""
+              className="sid-projects__image w-100 h-100 d-block"
+            />
+      </div>
+    </a>
+  );
+};
 const Projects = () => {
   return (
-    <section>
+    <section className="sid-projects">
       <Container>
         <Row>
           <Col xs={12}>
             <Swiper
-              modules={[Pagination]}
-              spaceBetween={50}
+              modules={[Autoplay, Pagination, EffectCoverflow]}
+              spaceBetween={0}
               slidesPerView={1}
               pagination={{ clickable: true }}
               className="sid-projects__slider"
+              loop={true}
+              autoplay={{
+                delay: 3500,
+                disableOnInteraction: false,
+              }}
             >
-              <SwiperSlide>
-                <div className="sid-projects__slider">
-                  Hello
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="sid-projects__slider">
-                Hi
-                </div>
-              </SwiperSlide>
+              {projectsData.map((data, index) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <SlideImage
+                      projectLink={data.projectLink}
+                      backgroundImage={data.backgroundImage}
+                    />
+                  </SwiperSlide>
+                );
+              })}
             </Swiper>
           </Col>
         </Row>
